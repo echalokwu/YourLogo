01d8308e23db39004f0df60cfd35107044203455
echalokwu <echalokwu@gmail.com>
2020-07-08 22:22:42 -0400

    test update

--- pages/loginPage.py
+++ pages/loginPage.py
@@ -7,10 +7,14 @@ class LoginPage:
     def __init__(self, driver):
         self.driver = driver
 
+        self.url = "http://automationpractice.com/index.php"
         self.signIn_link_xpath = Locators.signIn_link_xpath
         self.signIn_button_xpath = Locators.signIn_button_xpath
         self.login_textBox_id = Locators.login_textBox_id
         self.passWord_textBox_id = Locators.passWord_textBox_id
+
+    def load(self):
+        self.driver.get(self.url)
 
     def click_signIn_link(self):
         self.driver.find_element(By.XPATH, Locators.signIn_link_xpath).click()
--- tests/test_Case_2.py
+++ tests/test_Case_2.py
@@ -1,26 +1,13 @@
-import pytest
-from selenium import webdriver
+from utils import utils as utils
 from pages.loginPage import LoginPage
 import time
 
 
-@pytest.fixture
-def test_setUp():
-    global driver
-    driver = webdriver.Chrome(executable_path="/Users/echalo/Desktop/YourLogo/automation/drivers/chromedriver")
-    driver.get("http://automationpractice.com/index.php")
-    driver.implicitly_wait(10)
-    driver.maximize_window()
-    yield
-    driver.close()
-    driver.quit()
+def test_verify_login_with_valid_details(browser):
+    login = LoginPage(browser)
 
-
-def test_verify_login_with_valid_details(test_setUp):
-    login = LoginPage(driver)
+    login.load()
     login.click_signIn_link()
-    time.sleep(3)
-    login.enter_email("iwe43@gmail.com")
-    login.enter_password("moimoi123")
+    login.enter_email(utils.email)
+    login.enter_password(utils.password)
     login.click_signIn_button()
-    time.sleep(5)
